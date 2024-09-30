2474b77fa279b264eb44e0e539f01e88ceac06ff
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-10-01 00:48:49 +0530

    s

--- modules/lt/main.tf
+++ modules/lt/main.tf
@@ -15,14 +15,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   ip_protocol       = "tcp"
   to_port           = var.app_port
 }
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_ssh" {
   security_group_id = aws_security_group.allow_tls.id
   cidr_ipv4         = var.sg_ingress_cidr
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
 }
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_ssh" {
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
