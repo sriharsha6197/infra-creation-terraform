9e40471027006b36fcd91577b6baad37da8353f9
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-09-30 22:49:34 +0530

    getting all the values of pvt subnets

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,5 +2,8 @@ output "vpc_id" {
   value = aws_vpc.main.id 
 }
 output "PB_SUBNETs" {
-  value = aws_subnet.public_subnet.*.id
+  value = values(aws_subnet.public_subnet)[*].id
+}
+output "PVT-SUBNETs" {
+ value = values(aws_subnet.private_subnet)[*].id 
 }
