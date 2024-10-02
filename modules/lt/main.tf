adc64f0e8e6b77c0c09becefb0ee1928c4c1315a
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-10-02 20:46:37 +0530

    converting template file into base64 encode function

--- modules/lt/main.tf
+++ modules/lt/main.tf
@@ -33,9 +33,9 @@ resource "aws_launch_template" "foo" {
   image_id = data.aws_ami.example.id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  user_data = templatefile("${path.module}/userdata.sh",{
+  user_data = base64encode(templatefile("${path.module}/userdata.sh",{
     role_name = var.component
-  })
+  }))
   tag_specifications {
     resource_type = "instance"
     tags = {
