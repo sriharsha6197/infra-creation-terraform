af9f905f668b45fb2db1c7748d5047353486bf96
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-10-02 21:27:33 +0530

    applying role to frontend and backend instances

--- modules/lt/main.tf
+++ modules/lt/main.tf
@@ -86,6 +86,9 @@ resource "aws_launch_template" "foo" {
   user_data = base64encode(templatefile("${path.module}/userdata.sh",{
     role_name = var.component
   }))
+  iam_instance_profile {
+    name = aws_iam_instance_profile.test_profile.name
+  }
   tag_specifications {
     resource_type = "instance"
     tags = {
