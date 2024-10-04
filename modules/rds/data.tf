ac543651d021c762c0e1a173dad63129eacff1bf
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-10-04 11:03:38 +0530

    master password value retrieval in data.tf

--- modules/rds/data.tf
+++ modules/rds/data.tf
@@ -2,5 +2,5 @@ data "aws_ssm_parameter" "master_username" {
   name = "${var.env}.rds.master_username"
 }
 data "aws_ssm_parameter" "master_password" {
-  name = "{var.env}.rds.master_password"
+  name = "${var.env}.rds.master_password"
 }
