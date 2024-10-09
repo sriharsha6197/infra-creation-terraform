7153a873d6447396e7d456230e7c9eb81308b2d0
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-10-09 21:27:44 +0530

    installation of ansible

--- modules/lt/userdata.sh
+++ modules/lt/userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dnf install ansible -y | tee -a /tmp/userdata.log 
+sudo dnf install ansible -y | tee -a /tmp/userdata.log 
 dnf install python3.12-pip.noarch -y | tee -a /tmp/userdata.log 
 pip3.12 install botocore boto3  | tee -a /tmp/userdata.log
 ansible-pull -i localhost, -U https://github.com/sriharsha6197/expense-ansible.git -e ansible_user=Centos -e ansible_password=DevOps321 -e role_name=${role_name} -e env=${env} expense.yaml  | tee -a /tmp/userdata.log
